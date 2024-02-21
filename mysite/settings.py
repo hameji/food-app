1adc1f92b6a9e41655b1af6aa5525906b7873ecd
hameji <hameji@gmail.com>
2024-02-21 16:00:22 -0500

    added media root and path to settings

--- mysite/settings.py
+++ mysite/settings.py
@@ -131,6 +131,9 @@ STATIC_URL = '/static/'
 LOGIN_REDIRECT_URL = 'food:index'
 LOGIN_URL = 'login'
 
+MEDIA_ROOT = os.path.join(BASE_DIR, 'pictures')
+MEDIA_URL = '/pictures/'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
