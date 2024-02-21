05c461f1a30214e1536e79944e146ce43b07b9b3
hameji <hameji@gmail.com>
2024-02-21 00:13:39 -0500

    added login html

--- mysite/urls.py
+++ mysite/urls.py
@@ -24,6 +24,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('food/', include('food.urls')),
     path('register/', user_views.register, name='register'),
-    path('login/', authentication_views.LoginView.as_view(), name ='login'),
-    path('logout/', authentication_views.LogoutView.as_view(), name ='logout'),
+    path('login/', authentication_views.LoginView.as_view(template_name='users/login.html'), name ='login'),
+    path('logout/', authentication_views.LogoutView.as_view(template_name='users/logout.html'), name ='logout'),
 ]
