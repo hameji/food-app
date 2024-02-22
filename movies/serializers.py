c45899a9a8dea42449a8fdac32326664d9b85ce1
hameji <hameji@gmail.com>
2024-02-21 20:29:43 -0500

    added genre

--- movies/serializers.py
+++ movies/serializers.py
@@ -4,4 +4,4 @@ from .models import Moviedata
 class MovieSerializer(serializers.ModelSerializer):
     class Meta:
         model = Moviedata
-        fields = ['id', 'name', 'duration', 'rating', 'genre']
+        fields = ['id', 'name', 'genre', 'duration', 'rating']
