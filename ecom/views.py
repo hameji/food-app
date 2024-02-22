b69a1d07ec09e5f7a74ca72b32c089fd5ae02026
hameji <hameji@gmail.com>
2024-02-22 12:27:10 -0500

    made views for index

--- ecom/views.py
+++ ecom/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
+from .models import Product
 # Create your views here.
+
+def index(request):
+    product_objects = Product.objects.all()
+    return render(request, 'ecom/index.html', {'product_objects': product_objects})
