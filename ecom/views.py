5fb5ac3d2bc2758551dfe36d237891808ef642d8
hameji <hameji@gmail.com>
2024-02-22 12:40:00 -0500

    changed view def name to shop

--- ecom/views.py
+++ ecom/views.py
@@ -3,6 +3,6 @@ from django.shortcuts import render
 from .models import Product
 # Create your views here.
 
-def index(request):
+def shop(request):
     product_objects = Product.objects.all()
     return render(request, 'ecom/index.html', {'product_objects': product_objects})
