c11a25340062082a186718af644ca4409817fb93
hameji <hameji@gmail.com>
2024-02-21 20:11:29 -0500

    changed to singular

--- movies/serializers.py
+++ movies/serializers.py
@@ -1,7 +1,7 @@
 from rest_framework import serializers
 from .models import Moviedata
 
-class MovieSerializers(serializers.ModelSerializer):
+class MovieSerializer(serializers.ModelSerializer):
     class Meta:
         model = Moviedata
         fields = ['id', 'name', 'duration', 'rating']
