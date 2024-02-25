7aa6cd0fdd73e520793d6c8885dfc1fea35c3f60
Hajime Taniguchi <hameji@gmail.com>
2024-02-25 02:44:52 -0500

    changed models

--- facebook_crawler/models.py
+++ facebook_crawler/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Link(models.Model):
+
+    def __str__(self):
+        return self.name
+
+    name = models.CharField(max_length=1000, null=True, blank=True)
+    address = models.CharField(max_length=1000, null=True, blank=True)
