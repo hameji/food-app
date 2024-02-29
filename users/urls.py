8874d39e2b48df5a611b81312c8372e144ca163d
Hajime Taniguchi <hameji@gmail.com>
2024-02-29 17:42:19 -0500

    added new urls for password

--- users/urls.py
+++ users/urls.py
@@ -8,7 +8,13 @@ urlpatterns = [
     path('', user_views.index, name='index'),
     path('register/', user_views.register, name='register'),
     path('login/', authentication_views.LoginView.as_view(template_name='users/login.html'), name ='login'),
-    path('logout/', authentication_views.LogoutView.as_view(template_name='users/logout.html'), name ='logout'),
-    path('password_change/', authentication_views.PasswordChangeView.as_view(), name ='change_password'),
+    path('logout/', authentication_views.LogoutView.as_view(template_name='users/logout.html'), {'next_page': '/'}, name ='logout'),
+    path('password_change/', authentication_views.PasswordChangeView.as_view(), name ='password_change'),
+    path('password_change/done/', authentication_views.PasswordChangeDoneView.as_view(), name ='password_change_done'),
+    path('password_reset/', authentication_views.PasswordResetView.as_view(), name ='password_reset'),
+    path('password_reset/done/', authentication_views.PasswordResetDoneView.as_view(), name ='password_reset_done'),
+    path('password_reset/<uidb64>/<token>/', authentication_views.PasswordResetConfirmView.as_view(template_name="users/password_reset_confirm.html"), name ='password_reset_confirm'),
+    path('password_reset/complete/', authentication_views.PasswordResetCompleteView.as_view(template_name="users/password_reset_complete.html"), name ='password_reset_complete'),
     path('profile/', user_views.profilepage, name='profile'),
 ]
+
