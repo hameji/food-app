f3778501199b3968762333dc7b8c561fad0bde3c
Hajime Taniguchi <hameji@gmail.com>
2024-02-29 23:03:44 -0500

    users changed

--- users/views.py
+++ users/views.py
@@ -2,21 +2,33 @@ from django.contrib import messages
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
-from django.shortcuts import render, redirect
+from django.shortcuts import render
 
-from .forms import RegisterForm, LoginForm
+from .forms import RegisterForm, UserRegistrationForm, LoginForm
 
 def index(request):
     return render(request, 'users/index.html')
 
 def register(request):
+    use_custom = True
     if request.method == 'POST':
-        form = RegisterForm(request.POST)
-        if form.is_valid(): # checked unique user id
-            form.save()
-            username = form.cleaned_data.get('username')
-            messages.success(request, f'Welcome {username}, your account is created!')
-            return redirect('login')
+        if use_custom:
+            form = UserRegistrationForm(request.POST)
+            if form.is_valid():
+                new_user = form.save(commit=False)
+                new_user.set_password(form.cleaned_data['password'])
+                new_user.save()
+                return render(request, 'users/register_done.html')
+            else:
+                user_form = UserRegistrationForm()
+            return render(request, 'users/register.html', {'user_form': user_form})
+        else:
+            form = RegisterForm(request.POST)
+            if form.is_valid(): # checked unique user id
+                form.save()
+                username = form.cleaned_data.get('username')
+                messages.success(request, f'Welcome {username}, your account is created!')
+            return render(request, 'users/index.html')
     else:
         form = RegisterForm()
     return render(request, 'users/register.html', {'form': form})
@@ -41,3 +53,6 @@ def user_login(request):
     else:
         form = LoginForm()
         return render(request, 'users/login.html', {'form': form})
+
+def register_done(request):
+    return render(request, 'portfolio/index.html')
