d113d36aa7e032262bcd1c78ba371d6ac63e17e4
Hajime Taniguchi <hameji@gmail.com>
2024-03-01 20:11:44 -0500

    run npx init

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+
+module.exports = {
+  content: [
+    "./users/templates/users/base.html",
+
+],
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
